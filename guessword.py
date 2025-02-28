98097b05d2c696e460e3cb9d7ca64ec347aaecfd
FarooquiRaiyan <aammaannxyz@gmail.com>
2025-02-28 22:29:55 +0530

    guessword.py

--- guessword.py
+++ guessword.py
@@ -5,10 +5,21 @@ words=["hello","choice","good","bad","worst","think","dsa","raiyan"]
 rndm_guess=random.choice(words)
 print(rndm_guess)
 
-input_word=input("enter the word for guess")
 
-while (input_word!= 'Exit'):
+guess_count=0
+input_word=""
+
+while input_word!= 'Exit':
+    input_word=input("enter the word for guess")
     if input_word in  rndm_guess:
         print("you guess it write")
-    else:
-        print("sorry wrong")
+        exit()
+    elif input_word =='Exit':
+        print("Exiting")
+    elif input_word not in rndm_guess:
+        guess_count=guess_count+1
+        print("sorry try again your count is this ",guess_count)
+        if guess_count>12:
+            print("your guess count is 12  so exiting the program",guess_count)
+            exit()
+   
