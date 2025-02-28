4bf9077b779f4a805b2539fca49e3e7d6e019ebb
FarooquiRaiyan <aammaannxyz@gmail.com>
2025-02-28 22:09:10 +0530

    changing add

--- guessword.py
+++ guessword.py
@@ -4,3 +4,11 @@ words=["hello","choice","good","bad","worst","think","dsa","raiyan"]
 
 rndm_guess=random.choice(words)
 print(rndm_guess)
+
+input_word=input("enter the word for guess")
+
+while (input_word!= 'Exit'):
+    if input_word in  rndm_guess:
+        print("you guess it write")
+    else:
+        print("sorry wrong")
